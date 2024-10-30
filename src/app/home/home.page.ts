4a07f0f15a239e351b3eb4c534cae87b7463a6bd
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2024-10-30 19:15:08 +0600

    score updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonImg,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonImg, IonHeader, IonToolbar, IonTitle, IonContent, DecimalPipe],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  ballPosition = { x: 0, y: 0 };
+  score = 0;
+  ballSize = 150;
+  ballColor = 'bg-blue-500';
+  slapSound = new Audio('assets/slap-sound.mp3');
+  showSlapIcon = false;
+
+  ngOnInit() {
+    this.moveBall();
+  }
+
+  moveBall() {
+    setInterval(() => {
+      const screenHeight = window.innerHeight - this.ballSize;
+      const screenWidth = window.innerWidth - this.ballSize;
+
+      this.ballPosition = {
+        x: Math.random() * screenWidth,
+        y: Math.random() * screenHeight,
+      };
+    }, 1500);
+  }
+
+  onBallClick() {
+    this.score++;
+    this.playSlapSound();
+    this.changeBallColor();
+    this.showSlapEffect();
+  }
+
+  playSlapSound() {
+    this.slapSound.play();
+  }
+
+  changeBallColor() {
+    this.ballColor = 'bg-red-500';
+    setTimeout(() => {
+      this.ballColor = 'bg-blue-500';
+    }, 1000);
+  }
+
+  showSlapEffect() {
+    this.showSlapIcon = true;
+    setTimeout(() => {
+      this.showSlapIcon = false;
+    }, 1000);
+  }
 }
